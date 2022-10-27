137d1fd0806ad74a98d9a8b6b58ccca84813f584
unknown <nazmulnobel000@gmail.com>
2022-10-27 18:01:04 +0600

    check out page update

--- src/Course/Check.js
+++ src/Course/Check.js
@@ -4,10 +4,14 @@ import { AuthContext } from '../Context/AuthProvider/AuthProvider';
 
 const Check = ({children}) => {
     const {user} = useContext(AuthContext);
+    console.log(user);
     return (
         <div className='m-5 border p-3'>
             <h1 className='text-primary'>Thank You For Check Up</h1>
             <p className='text-secondary'>Your Email is: {user?.email}</p>
+            <p className='text-secondary'>Your Name is: {user?.displayName}</p>
+            <p className='text-secondary'>Verified your email: {user?.emailVerified}</p>
+            <p className='text-secondary'>Your Photo Url is: {user?.photoURL}</p>
         </div>
     );
 };
